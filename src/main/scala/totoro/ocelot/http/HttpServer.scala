d4069e0e54bbd7f2e177e314872147d8195ef16d
MoonlightOwl <murky.owl@gmail.com>
2017-10-09 01:11:57 +0200

    test websocket server

--- src/main/scala/totoro/ocelot/http/HttpServer.scala
+++ src/main/scala/totoro/ocelot/http/HttpServer.scala
@@ -3,23 +3,38 @@ package totoro.ocelot.http
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.model.ws.{BinaryMessage, Message, TextMessage}
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.{Flow, Sink, Source}
 import totoro.ocelot.Config
 
 import scala.concurrent.{ExecutionContextExecutor, Future}
 
 /**
-  * Share SPA web page with OpenComputers terminal window
+  * Host the main webpage with emulator window
   */
 
 class HttpServer(implicit val system: ActorSystem, implicit val materializer: ActorMaterializer) {
   implicit val executionContext: ExecutionContextExecutor = system.dispatcher
 
+  val testWebsocketHandler: Flow[Message, Message, Any] =
+    Flow[Message].mapConcat {
+      case tm: TextMessage =>
+        TextMessage(Source.single("Hello ") ++ tm.textStream ++ Source.single("!")) :: Nil
+      case bm: BinaryMessage =>
+        bm.dataStream.runWith(Sink.ignore)
+        Nil
+    }
+
   val route: Route = get {
     (pathEndOrSingleSlash & redirectToTrailingSlashIfMissing(StatusCodes.TemporaryRedirect)) {
       getFromResource("index.html")
+    } ~ path("websocket") {
+      get {
+        handleWebSocketMessages(testWebsocketHandler)
+      }
     } ~ {
       getFromResourceDirectory("")
     }
